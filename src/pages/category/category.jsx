ffce5e58e3a4e4f56628847e5339c5646dd5562f
Dade <1095412419@qq.com>
2020-10-23 19:20:57 +0800

    完善一级二级列表显示

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -3,10 +3,11 @@ import{
     Card,
     Table,
     Button,
-    message
+    message,
+    Modal
 }from 'antd'
 
-import {PlusOutlined } from '@ant-design/icons';
+import {PlusOutlined,DoubleRightOutlined} from '@ant-design/icons';
 import LinkButton from '../../compmont/link-button'
 import {reqCategorys} from '../../api'
 
@@ -16,6 +17,9 @@ export default class Category extends Component {
     state = {
         loading:false, //是否正在获取数据
         categorys:[], //一级分类列表 dataSourse
+        subCategorys:[], //二级分类列表
+        parentId:0, //当前需要显示的分类列表的父分类ID
+        parentName: ''//当前需要显示的分类列表的父分类名称
     }
     
     /**
@@ -30,10 +34,12 @@ export default class Category extends Component {
             {
                 title: '操作',
                 width:300,
-                render: () => (     //返回需要显示的界面标签
+                render: (category) => (     //返回需要显示的界面标签
                     <span>
                         <LinkButton>修改分类</LinkButton>
-                        <LinkButton>查看子分类</LinkButton>
+                        {/*如何向事件回调函数传递参数：先定义一个匿名函数，在函数调用处理的函数并传入数据 */}
+                        {this.state.parentId=== 0 ? <LinkButton onClick={() => this.showSubCategorys(category)}>查看子分类</LinkButton> : null} {/*点击的时候才调用*/}
+                        {/* <LinkButton onClink={this.showSubCategorys(category)}>查看子分类</LinkButton> //渲染的时候就被调用了 */}
                     </span>
                 )
               },
@@ -41,27 +47,64 @@ export default class Category extends Component {
     }
 
     /**
-     *异步获取一级分类列表显示 
+     *异步获取一级/二级分类列表显示 
      */
     getCategorys = async () =>{
         //发请求前 显示loading
         this.setState({loading:true})
+        const {parentId} = this.state
         //发送异步ajax请求 获取数据
-        const result = await reqCategorys(0)    //await promise对象
+        const result = await reqCategorys(parentId)    //await promise对象
         //请求完成后，隐藏loading
         this.setState({loading:false})
         const response = result.data
         if(response.status === 0){
+            //取出分类数组 （一级/二级
             const categorys = response.data
-            //更新状态
-            this.setState({
-                categorys
-            })
+            if(parentId === 0){
+                //更新一级分类状态
+                this.setState({
+                    categorys:categorys
+                })
+            }else{
+                //更新二级分类状态
+                this.setState({
+                    subCategorys:categorys
+                })
+            }
+           
         }else{
             message.error('获取分类列表失败')
         }
     }
+    /**
+     * 显示指定一级分类列表
+     */
+    showCategorys = ()=>{
+        //更新为一级列表状态
+        this.setState({
+            parentId:0,
+            parentName:'',
+            subCategorys:[],
+        })
+    }
 
+    /**
+     * 显示指定一级分类对象的二级子列表
+     */
+    showSubCategorys = (category) =>{
+        // 更新state (setstate是异步更新)
+        // setState(updater, [callback])    https://zh-hans.reactjs.org/docs/react-component.html#setstate
+        this.setState({
+            parentId:category.id,
+            parentName:category.name,
+        },()=>{ //该回调函数，在状态更新且重新render()后执行
+            // console.log('parentId:', this.state.parentId)
+            //获取二级分类列表
+            this.getCategorys()
+        })
+        //setState()不能立即获取最新的状态 因为setState()是异步更新状态的
+    }
 
     /**
      * 为第一次render()准备数据
@@ -74,16 +117,23 @@ export default class Category extends Component {
      * 执行异步任务 发送ajax请求
      */
     componentDidMount(){
+        //获取一级分类列表
         this.getCategorys();
     }
 
     render() {  //放入render里 每次重新render 都会重新创建 所以columns不放入render
 
         //读取state数据
-        const {categorys, loading} = this.state
+        const {categorys, loading, subCategorys, parentId, parentName} = this.state
 
         //card 左侧
-        const title = '一级分类列表'
+        const title = parentId === 0? <LinkButton>一级分类列表</LinkButton>: (
+            <span>
+                <LinkButton onClick={this.showCategorys}>一级分类列表</LinkButton>
+                <DoubleRightOutlined style={{marginRight:5}}/>
+                <span>{parentName}</span>
+            </span>
+        )
         //card 右侧
         const extra = (
             <Button type='primary'>
@@ -99,7 +149,7 @@ export default class Category extends Component {
                     bordered = {true}
                     loading = {loading}
                     rowKey = '_id'
-                    dataSource={categorys} //数组变量
+                    dataSource={parentId===0? categorys: subCategorys} //数组变量
                     columns={this.columns} 
                     pagination={{defaultPageSize:5, showQuickJumper:true}}
                     />;   
